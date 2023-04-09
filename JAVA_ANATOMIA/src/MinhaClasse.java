5b2760b8d5f8ace70b28f0f0cfa051b13c018230
RENNER AUGUSTO ASSUNCAO MILANO <105928063+MilanoRenner@users.noreply.github.com>
2023-04-09 15:00:08 -0300

    Update 2 curso Java

---
+++ JAVA_ANATOMIA/src/MinhaClasse.java
@@ -0,0 +1,11 @@
+public class MinhaClasse {
+    
+    public static void main(String[] args) throws Exception 
+    
+    {
+        
+        System.out.println("Hello, World!");
+    
+    }
+
+}
